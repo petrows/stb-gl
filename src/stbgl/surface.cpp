1c7472ade99ab8c03a63d0f336622124765ea34e
Petro <petro@petro.ws>
2016-02-27 21:13:50 +0300

    syntax sugar

--- src/stbgl/surface.cpp
+++ src/stbgl/surface.cpp
@@ -33,6 +33,10 @@ surface_t::surface_t(uint32_t w, uint32_t h, GLuint texture)
 		// Poor filtering. Needed !
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	} else {
+		glBindTexture(GL_TEXTURE_2D, _texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	}
 
 	// Set "renderedTexture" as our colour attachement #0
@@ -58,13 +62,12 @@ surface_ptr_t surface_t::create(uint32_t w, uint32_t h)
 	return surface_ptr_t(new surface_t(w, h, 0));
 }
 
-surface_ptr_t surface_t::create(uint32_t w, uint32_t h, GLuint texture)
+surface_ptr_t surface_t::create(uint32_t w, uint32_t h, GLuint &texture)
 {
 	surface_t * s = new surface_t(w, h, 0);
 	s->set_current();
 	texture_t tex(w, h);
 	tex.draw(texture, 0, 0, w, h);
-
 	return surface_ptr_t(s);
 }
 
@@ -72,10 +75,12 @@ surface_ptr_t surface_t::create_from_image(const char *path)
 {
 	uint32_t w, h;
 	GLuint texture = texture_t::load_file_to_texture(path, w, h);
-	return create(w, h, texture);
+	surface_ptr_t out = create(w, h, texture);
+	glDeleteTextures(1, &texture);
+	return out;
 }
 
-void surface_t::fill_rect(uint32_t color_rgba, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
+surface_t& surface_t::fill_rect(uint32_t color_rgba, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
 {
 	set_current();
 
@@ -85,23 +90,35 @@ void surface_t::fill_rect(uint32_t color_rgba, uint32_t x, uint32_t y, uint32_t 
 
 	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
 	{
-		cerr << "Framebuffer errir!!!" << endl;
+		throw framebuffer_error_t(string("glCheckFramebufferStatus = ") + std::to_string(glCheckFramebufferStatus(GL_FRAMEBUFFER)));
 	}
+
+	return *this;
 }
 
-void surface_t::blit(surface_ptr_t &surface, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
+surface_t& surface_t::blit(surface_ptr_t &surface, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
 {
 	if (!w) w = surface->width();
 	if (!h) h = surface->width();
-	set_current();
-	texture_t tex(_width, _height);
-	tex.draw(surface->get_texture(), x, y, w, h);
+
+	return blit(surface->get_texture(), x, y, w, h);
 }
 
-bool surface_t::set_current()
+surface_t &surface_t::blit(GLuint texture, uint32_t x, uint32_t y, uint32_t w, uint32_t h)
+{
+	set_current();
+	texture_t tex(_width, _height);
+	tex.draw(texture, x, y, w, h);
+
+	return *this;
+}
+
+surface_t& surface_t::set_current()
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, _framebuffer);
 	glBindTexture(GL_TEXTURE_2D, _texture);
 
 	glViewport(0, 0, _width, _height);
+
+	return *this;
 }
--- src/stbgl/surface.h
+++ src/stbgl/surface.h
@@ -17,15 +17,16 @@ public:
 	~surface_t();
 
 	static surface_ptr_t create(uint32_t w, uint32_t h); //! Creates new surface object
-	static surface_ptr_t create(uint32_t w, uint32_t h, GLuint texture); //! Creates new surface object using existing texture
+	static surface_ptr_t create(uint32_t w, uint32_t h, GLuint &texture); //! Creates new surface object using existing texture
 	static surface_ptr_t create_from_image(const char *path); //! Creates new surface object using image
 
-	void clear(uint32_t color_rgba) { fill_rect(color_rgba, 0, 0, _width, _height); }
-	void fill_rect(uint32_t color_rgba, uint32_t x, uint32_t y, uint32_t w, uint32_t h);
-	void blit(surface_ptr_t &surface, uint32_t x, uint32_t y, uint32_t w = 0, uint32_t h = 0);
+	void clear(uint32_t color_rgba) { fill_rect(color_rgba, 0, 0, _width, _height); } //! Fill all surface with color
+	surface_t &fill_rect(uint32_t color_rgba, uint32_t x, uint32_t y, uint32_t w, uint32_t h); //! Fill desired rectangle surface with color
+	surface_t &blit(surface_ptr_t &surface, uint32_t x, uint32_t y, uint32_t w = 0, uint32_t h = 0); //! Blit other surface to our
+	surface_t &blit(GLuint texture, uint32_t x, uint32_t y, uint32_t w = 0, uint32_t h = 0); //! Blit other surface to our
 
-	GLuint get_texture() { return _texture; }
-	bool set_current(); //! Set this surface 'current' for drawing
+	GLuint get_texture() { return _texture; } //! get current framebuffer texture
+	surface_t &set_current(); //! Set this surface 'current' for drawing
 
 	uint32_t width() { return _width; }
 	uint32_t height() { return _height; }
